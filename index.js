fca2f4b068911876373ae8affbbf1d5b63015199
Ashutosh Galgali <amgalgali@gmail.com>
2020-02-18 16:05:27 +0530

    Renamed images to match ids. Added logic to toggle clicked state image.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-document.querySelector('.Bloodfall-img').addEventListener('click', function(){
-  
+document.querySelector('#bloodfall').addEventListener('click', function(){
+  x = document.querySelector('.yes');
+  y = x.getAttribute('id');
+  x.classList.remove('yes');
+  y = y + '.png'
+  console.log(y);
+  x.setAttribute('src', 'images/'+y);
+  this.classList.add("yes");
+  this.setAttribute('src', 'images/bloodfall-y.png');
 });
